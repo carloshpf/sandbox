ad04dbc5ed4ad73e8004635ff4b2bd2a102513b6
Carlos Porto Filho <carloshpf@gmail.com>
2015-10-28 15:26:03 -0200

    Add the tutorial and the assignment 3

--- R/coursera/assignment_3.R
+++ R/coursera/assignment_3.R
@@ -1,8 +1,30 @@
+setwd("C:/Users/carlos/Repo/sandbox/R/coursera/")
+
 download.file("https://github.com/DanieleP/PA3-tutorial/raw/master/cities.csv", destfile = "cities.csv", method = "curl")
 download.file("https://github.com/DanieleP/PA3-tutorial/raw/master/citiesNA.csv", destfile = "citiesNA.csv", method = "curl")
 
 dir()
 
+
 data <- read.csv("cities.csv")
 
 # Order a data frame by the values in its columns
+
+# Retorna os indices ordenados da primeira coluna
+order(data[, 1])
+
+# Faz um subset de data usando os indices retornados por order
+data[order(data[, 1]), ]
+# O subset funciona assim:
+data[c(3, 5), ]
+# cities countries areakm2 populationk
+# 3    NYC       USA    1214        8406
+# 5 London        UK    1737        9789
+
+sortByColumn <- function(data, column) {
+  orderData <- data[order(data[, column]), ]
+  return(orderData)
+}
+
+
+
