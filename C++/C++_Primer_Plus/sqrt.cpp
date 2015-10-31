b3c8a008d682e4f84c5657df1dff34b71552a1f5
Carlos Porto Filho <carloshpf@gmail.com>
2015-10-31 17:43:56 -0200

    Create a folder to hold code from the book C++ Primer Plus

---
+++ C++/C++_Primer_Plus/sqrt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	using namespace std;
+
+	double area;
+	cout << "Enter the area: ";
+	cin >> area;
+	double side;
+	cout << "That's the equivalent of a square " << side << endl;
+	return 0;
+}
---
+++ C++/C++_Primer_Plus/testchar.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  char dog[8] = {'b', 'e', 'a', 'u', 'x', ' ', 'I', 'I'};
+  char cat[5] = {'f', 'a', 't', 's', '\0'};
+  std::cout << dog;
+  std::cout << cat;
+  return 0;
+}
